27f8070ddbf4aef5a0e5ffb6436c22b048f73462
afrin823 <afrin12490@gmail.com>
2024-06-06 08:41:45 +0600

    package lock.json porblem solve

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
-//TourismManagement --- name
-//cNOQOxHnkX4nyoVI --- psw
 
 
 
@@ -38,13 +35,26 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
-
+    app.get('/country/one/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await countryCollection.findOne(query);
+      res.send(result);
+    })
     app.post('/country', async(req, res) => {
       const newCountry = req.body;
       console.log(newCountry)
       const result = await countryCollection.insertOne(newCountry);
       res.send(result)
     })
+    app.get('/countries/:email', async(req, res) => {
+      const email = req.params.email;
+      const query = {email: email}
+      const cursor = countryCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
